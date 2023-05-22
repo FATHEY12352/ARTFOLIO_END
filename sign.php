39172e0a2aa37a43793fa6108155aa357031eb62
FATHEY12352 <40392321+FATHEY12352@users.noreply.github.com>
2023-05-22 08:57:35 +0500

    Update sign.php

--- sign.php
+++ sign.php
@@ -1,29 +1,57 @@
 <?php
    
-session_start();
-if(isset($_SESSION["user"])){
-  header("Location: dashboard");
-}
 
-if(isset($_post["submit"])){
-  //================
-  $user=$_POST['user'];
-  $pass=$_POST['pass'];
-
-  //$con=mysqli_connect("localhost","root","root","auth");
-
-  // make tow string for tests\
-  // and add your users
-  // see datebase cots
-
-  $sql="INSERT INTO users(name,password) VALUES('$user','$pass')";
-
+   ob_start();
+   include('header.php');
+   include_once("sign/db_connect.php");
+   session_start();
    
-
-  mysqli_query($con,$sql);
-  mysqli_close($con);
-  header("Location: index.php");
+   $error = false;
+   if (isset($_POST['signup'])) {
+       $name = mysqli_real_escape_string($conn, $_POST['name']);
+       $email = mysqli_real_escape_string($conn, $_POST['email']);
+       $password = mysqli_real_escape_string($conn, $_POST['password']);
+       $cpassword = mysqli_real_escape_string($conn, $_POST['cpassword']);	
+       if (!preg_match("/^[a-zA-Z ]+$/",$name)) {
+           $error = true;
+           $uname_error = "Name must contain only alphabets and space";
+       }
+       if(!filter_var($email,FILTER_VALIDATE_EMAIL)) {
+           $error = true;
+           $email_error = "Please Enter Valid Email ID";
+       }
+       if(strlen($password) < 6) {
+           $error = true;
+           $password_error = "Password must be minimum of 6 characters";
+       }
+       if($password != $cpassword) {
+           $error = true;
+           $cpassword_error = "Password and Confirm Password doesn't match";
+       }
+       if (!$error) {
+           if(mysqli_query($conn, "INSERT INTO users(user, email, pass) VALUES('" . $name . "', '" . $email . "', '" . md5($password) . "')")) {
+               $success_message = "Successfully Registered! <a href='login.php'>Click here to Login</a>";
+           } else {
+               $error_message = "Error in registering...Please try again later!";
+           }
+    }
 }
+
+// session_start();
+// if(isset($_SESSION['user_id'])!="") {
+// 	header("Location: index.php");
+// }
+// if (isset($_POST['login'])) {
+	
+// 	$result = mysqli_query($conn, "SELECT * FROM users WHERE email = '" . $email. "' and pass = '" . md5($password). "'");
+// 	if ($row = mysqli_fetch_array($result)) {
+// 		$_SESSION['user_id'] = $row['uid'];
+// 		$_SESSION['user_name'] = $row['user'];		
+// 		header("Location: index.php");
+// 	} else {
+// 		$error_message = "Incorrect Email or Password!!!";
+// 	}
+// }
 
 $user=$_POST['user'];
 $pass=$_POST['pass'];
@@ -42,6 +70,10 @@ if($user==="fathey"&&$pass==="123654"){
     elseif($user==="bassem"&&$pass==="123654"){
         header("Location: protofly_user_b.html");
     }
+
+    elseif($user==="adham"&&$pass==="123654"){
+        header("Location: protofly_user_l.html");
+    }
 ?>
 
 <!DOCTYPE html>
@@ -59,15 +91,18 @@ if($user==="fathey"&&$pass==="123654"){
 <body>
     <div class="container">
         <div class="signin-signup">
-            <form action="sign.php" method="post" class="sign-in-form">
+            <form role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="loginform">
                 <h2 class="title">Sign in</h2>
+
                 <div class="input-field">
-                    <i class="fas fa-user"></i>
-                    <input type="text" placeholder="Username" name="user">
+                    <i class="fas fa-envelope"></i>
+                    <input type="text" placeholder="email" name="user">
                 </div>
+
+
                 <div class="input-field">
                     <i class="fas fa-lock"></i>
-                    <input type="password" placeholder="Password" name="pass">
+                    <input type="password" placeholder="password" name="pass">
                 </div>
 
                 <?php
@@ -89,12 +124,7 @@ if($user==="fathey"&&$pass==="123654"){
 
                 ?>
 
-
-                <input type="submit" value="Login" class="btn" >
-
-                
-
-
+                <input type="submit" name="login" value="Login" class="btn">
 
                 <p class="social-text">Or Sign in with social platform</p>
                 <div class="social-media">
@@ -113,21 +143,44 @@ if($user==="fathey"&&$pass==="123654"){
                 </div>
                 <p class="account-text">Don't have an account? <a href="#" id="sign-up-btn2">Sign up</a></p>
             </form>
-            <form action="" class="sign-up-form">
+            <span class="text-danger"><?php if (isset($error_message)) { echo $error_message; } ?></span>
+
+
+
+            <form role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="sign-up-form">
                 <h2 class="title">Sign up</h2>
+
+                
+
                 <div class="input-field">
                     <i class="fas fa-user"></i>
-                    <input type="text" placeholder="Username">
-                </div>
+                    <input type="text" name="name" placeholder="Enter Full Name" required value="<?php if($error) echo $name; ?>" class="form-control" />
+                    <span class="text-danger"><?php if (isset($uname_error)) echo $uname_error; ?></span>
+				</div>
+
+                
                 <div class="input-field">
                     <i class="fas fa-envelope"></i>
-                    <input type="text" placeholder="Email">
-                </div>
+					<input type="text" name="email" placeholder="Email" required value="<?php if($error) echo $email; ?>" class="form-control" />
+					<span class="text-danger"><?php if (isset($email_error)) echo $email_error; ?></span>
+				</div>
+
+
                 <div class="input-field">
-                    <i class="fas fa-lock"></i>
-                    <input type="password" placeholder="Password">
-                </div>
-                <input type="submit" value="Sign up" class="btn">
+                  <i class="fas fa-lock"></i>
+					<input type="password" name="password" placeholder="Password" required class="form-control" />
+					<span class="text-danger"><?php if (isset($password_error)) echo $password_error; ?></span>
+				</div>
+
+
+                <div class="input-field">
+                 <i class="fas fa-lock"></i>
+					<input type="password" name="cpassword" placeholder="Confirm Password" required class="form-control" />
+					<span class="text-danger"><?php if (isset($cpassword_error)) echo $cpassword_error; ?></span>
+				</div>
+
+                <input type="submit" name="signup" value="Sign Up" class="btn" />
+
                 <p class="social-text">Or Sign in with social platform</p>
                 <div class="social-media">
                     <a href="#" class="social-icon">
@@ -144,8 +197,14 @@ if($user==="fathey"&&$pass==="123654"){
                     </a>
                 </div>
                 <p class="account-text">Already have an account? <a href="#" id="sign-in-btn2">Sign in</a></p>
+
+
             </form>
         </div>
+
+
+
+
         <div class="panels-container">
             <div class="panel left-panel">
                 <div class="content">
